0df9109eae6cab6142278f286ce6eeedc6d5ef87
Syed Hassan <syedhassan@Syeds-MacBook-Air.local>
2023-06-30 01:10:42 +0530

    Deletion

--- flightbooking/flights/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Flight
-
-class FlightForm(forms.ModelForm):
-    class Meta:
-        model = Flight
-        fields = ('number', 'departure_time', 'seat_count')
