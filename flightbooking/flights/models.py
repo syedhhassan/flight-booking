258f574b136cda8ed01a68d809cb13d3710d17be
Syed Hassan <syedhassan@Syeds-MacBook-Air.local>
2023-06-30 00:04:43 +0530

    Essentials

--- flightbooking/flights/models.py
+++ flightbooking/flights/models.py
@@ -1,19 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Flight(models.Model):
-    flight_number = models.CharField(max_length=10)
-    date = models.DateField()
-    time = models.TimeField()
-    seats = models.IntegerField(default=60)
+    number = models.CharField(max_length=10)
+    departure_time = models.DateTimeField()
+    seat_count = models.PositiveIntegerField(default=60)
 
     def __str__(self):
-        return f"Flight {self.flight_number} - {self.date} {self.time}"
+        return self.number
+
+    def get_available_seats(self):
+        bookings = self.bookings.count()
+        return max(0, self.seat_count - bookings)
+
+    def is_full(self):
+        return self.get_available_seats() == 0
+
 
 class Booking(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
-    date = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE, related_name='bookings')
 
     def __str__(self):
-        return f"Booking {self.id} - {self.user.username} ({self.flight.flight_number})"
+        return f"Booking #{self.id} - User: {self.user.username}, Flight: {self.flight.number}"
