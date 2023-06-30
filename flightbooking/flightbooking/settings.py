e45a101b6ba8786206f2683a6e54b3241accbf9c
Syed Hassan <syedhassan@Syeds-MacBook-Air.local>
2023-06-30 08:49:55 +0530

    Screenshots added

--- flightbooking/flightbooking/settings.py
+++ flightbooking/flightbooking/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hu8u4qrf8xge34kymez=yt_7e93y3yep9q(=ic6^qc41g)vddp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
